893583031e6e4efdaad6f8715ce667bebfd70b59
Ramirez-Elias <ram16032@byui.edu>
2019-05-08 17:32:21 -0500

    Made changes to main Index file for assignment

---
+++ index.php
@@ -0,0 +1,63 @@
+<?php $ptitle='Home'; include'Week02\personalHomepage\modules\_head.php'; ?>
+<?php include'Week02\personalHomepage\modules\_header.php'; ?>
+<?php include'Week02\personalHomepage\modules\_nav.php'; ?>
+
+    
+        <div class="index">
+            <h1>CS313 - Homepage</h1>
+        </div>   
+        <div class="welcomeContent">
+          <ul>
+              <li>Week 3 Assignments 
+                 <ul> 
+                    <li><a href="#">Team Activity</a></li>
+                    <li><a href="#">03 Prove: Assignment - Shopping Cart</a></li>
+                 </ul>
+              <li>Week 4 Assignments 
+                 <ul> 
+                    <li><a href="#">Team Activity</a></li>
+                    <li><a href="#">04 Prove: Assignment - DB Setup</a></li>
+                 </ul>
+              <li>Week 5 Assignments 
+                 <ul> 
+                    <li><a href="#">Team Activity</a></li>
+                    <li><a href="#">05 Prove: Assignment - DB Access</a></li>
+              </ul>
+              <li>Week 6 Assignments 
+                 <ul> 
+                    <li><a href="#">Team Activity</a></li>
+                    <li><a href="#">06 Prove : Assignment - DB Update</a></li>
+                 </ul>
+              <li>Week 7 Assignments 
+                 <ul> 
+                    <li><a href="#">Team Activity</a></li>
+                    <li><a href="#">07 Prove: Assignment - Project 1</a></li>
+                 </ul>
+              <li>Week 8 Assignments 
+                 <ul> 
+                    <li><a href="#">08 Prove: Assignment - Hello World</a></li>
+                 </ul>     
+              <li>Week 9 Assignments 
+                 <ul> 
+                    <li><a href="#">09 Prove: Assignment - Postal Rate Calculator</a></li>
+                 </ul>   
+              <li>Week 10 Assignments 
+                 <ul> 
+                    <li><a href="#">10 Prove: Assignment - Milestone 1</a></li>
+                 </ul>
+              <li>Week 11 Assignments 
+                 <ul> 
+                    <li><a href="#">11 Prove: Assignment - Milestone 2</a></li>
+                 </ul>
+              <li>Week 12 Assignments 
+                 <ul> 
+                    <li><a href="#">12 Prove: Assignment - Milestone 3</a></li>
+                 </ul>
+              <li>Week 13-14 Assignments 
+                 <ul> 
+                    <li><a href="#">14 Prove: Project 2</a></li>
+                 </ul>
+          </ul>
+        </div>
+    </section>
+ <?php include'Week02\personalHomepage\modules\_footer.php'; ?>
