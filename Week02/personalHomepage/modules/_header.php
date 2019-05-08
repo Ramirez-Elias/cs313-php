a0d7ef90f820bfb8dd2383a679675802df9f5cb5
Ramirez-Elias <ram16032@byui.edu>
2019-05-08 18:02:14 -0500

    made changes to include new about page location

--- Week02/personalHomepage/modules/_header.php
+++ Week02/personalHomepage/modules/_header.php
@@ -1,6 +1,6 @@
   <body>
     <header>
-        <img src="Week02/personalHomepage/images/logo.jpg" alt="The site logo" width="150">
+        <img src="Week02/personalHomepage/images/Logo.jpg" alt="The site logo" width="150">
         <h1>Elias Ramirez</h1>
         <div class="subheading">CS313 Homepage</div>    
     </header>
