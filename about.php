8c10cfdd50ab08a6bbcd70c21b92c8050494df1f
Ramirez-Elias <ram16032@byui.edu>
2019-05-08 18:00:09 -0500

    trying to make Index fully work

---
+++ about.php
@@ -0,0 +1,18 @@
+<?php $ptitle='About Me'; include'Week02/personalHomepage/modules/_head.php'; ?>
+<?php include'Week02/personalHomepage/modules/_header.php'; ?>
+<?php include'Week02/personalHomepage/modules/_nav.php'; ?>
+
+        <h2>Hi! I'm Elias...</h2>
+        <div class="grid">
+        <p>I’m a Software Engineer Major that loves spending time with his family, traveling the world, build profitable business and build things with my own hands.</p>
+        
+        <picture class="mainPic">
+            <source media="(max-width: 400px)" srcset="images/philadelphia.jpg">
+              <source media="(max-width: 600px)" srcset="images/philadelphia.jpg">
+            <source media="(max-width: 800px)" srcset="images/philadelphia.jpg">                
+
+            <img src="images/philadelphia.jpg" alt="family vacation"> 
+        </picture>
+         <div>
+             
+   <?php include'modules/_footer.php'; ?>
